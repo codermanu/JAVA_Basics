df92eb9e176dd724f1a5d2cc904fa6c363a56f4f
codermanu <manu.nevish@gmail.com>
2019-06-03 22:27:50 +0530

    Print the sum of two numbers and declare more than one variable of the same type

--- Variables.java
+++ Variables.java
@@ -11,6 +11,20 @@ public class Variables{
         boolean myBool = true;
         System.out.println(myBool);
         System.out.println("Hello "+name+" who is "+number+" years old.");
-        
+
+        //Combine two variables
+        String firstName = "Nadeesha ";
+        String lastName = "Karunarathne";
+        String fullName = firstName + lastName;
+        System.out.println(fullName);
+
+        //Print the sum of two numbers
+        int a = 23;
+        int b = 13;
+        System.out.println(a+b);
+
+        //declare more than one variable of the same type
+        int x=3, y=6, z=9;
+        System.out.println(x+y+z);
     }
 }
