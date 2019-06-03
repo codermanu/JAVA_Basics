0d6338348331e88739435e3e4df812e5f1a3c16e
codermanu <manu.nevish@gmail.com>
2019-06-03 21:33:22 +0530

    Add a multi line comment

--- Comments.java
+++ Comments.java
@@ -2,5 +2,7 @@ public class Comments{
     public static void main(String[] args){
         //this is a single line comment
         System.out.println("Hello World");
+        /*This is a double line comment
+        this explains how to use comments*/
     }
 }
