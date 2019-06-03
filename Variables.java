16d4f49ba572ff34a5b2edbb6b47c2b510a03bf5
codermanu <manu.nevish@gmail.com>
2019-06-03 21:52:54 +0530

    Remove inverted commas in int variable value

--- Variables.java
+++ Variables.java
@@ -2,7 +2,7 @@ public class Variables{
     public static void main(String[] args){
         String name = "Nadeesha"; //String variable
         System.out.println(name);
-        int number ="10"; //int variable
+        int number = 10; //int variable
         System.out.println(number);
     }
 }
