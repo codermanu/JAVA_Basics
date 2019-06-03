4fa9ef4f7e0eb16ca9416dd92029adc4591fd83a
codermanu <manu.nevish@gmail.com>
2019-06-03 22:09:37 +0530

    Add other types of Variables

--- Variables.java
+++ Variables.java
@@ -4,5 +4,12 @@ public class Variables{
         System.out.println(name);
         int number = 10; //int variable
         System.out.println(number);
+        float myFloatNum = 5.99f;
+        System.out.println(myFloatNum);
+        char myLetter = 'D';
+        System.out.println(myLetter);
+        boolean myBool = true;
+        System.out.println(myBool);
+        
     }
 }
