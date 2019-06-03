c1b42c2be02638be6fdd6a50a3de4b4312168640
codermanu <manu.nevish@gmail.com>
2019-06-03 21:50:21 +0530

    Add an int variable

--- Variables.java
+++ Variables.java
@@ -1,6 +1,8 @@
 public class Variables{
     public static void main(String[] args){
-        String name = "Nadeesha";
+        String name = "Nadeesha"; //String variable
         System.out.println(name);
+        int number ="10"; //int variable
+        System.out.println(number);
     }
 }
