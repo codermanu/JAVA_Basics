24332aa54ed1b3a99ce4bdffe2eb0529a7e605cd
codermanu <manu.nevish@gmail.com>
2019-06-04 14:35:08 +0530

    Check if else if condition

---
+++ IfCondition.java
@@ -0,0 +1,13 @@
+public class IfCondition{
+    public static void main(String[] args) {
+      int age = 30;
+      if (age < 28) {
+        System.out.println("You are suitable for the job.");
+      } else if (age < 20) {
+        System.out.println("You are too young for the job, try in the future.");
+      }  else {
+        System.out.println("You are too old for the job, We are sorry!.");
+      }
+    }
+  }
+  
