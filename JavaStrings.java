02b3ab72c793bb8f69fa2cf2baf3c7e95db475d6
codermanu <manu.nevish@gmail.com>
2019-06-04 13:16:41 +0530

    Print the length of a given text

---
+++ JavaStrings.java
@@ -0,0 +1,9 @@
+public class JavaStrings{
+    public static void main(String[] args){
+        String name = "My name is Nadeesha Madhushani";
+        System.out.println(name);
+        //Prints the length of the given text
+        System.out.println("The length of the text given is "+name.length());
+
+    }
+}
