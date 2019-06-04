be14c1de1a5fa9b6685d71b6597bdd67af45eaaf
codermanu <manu.nevish@gmail.com>
2019-06-04 14:09:56 +0530

    Find the minimum number

--- MathClass.java
+++ MathClass.java
@@ -2,5 +2,8 @@ public class MathClass{
     public static void main(String[] args){
         //Print the maximum number
         System.out.println(Math.max(3, 9));
+
+        //Print the minimum number
+        System.out.println(Math.min(3, 9));
     }
 }
