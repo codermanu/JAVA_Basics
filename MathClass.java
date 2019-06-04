ee51b5de34cd4fb3918401d646ddb0da22e1a1dc
codermanu <manu.nevish@gmail.com>
2019-06-04 14:12:55 +0530

    Find the sqaure root of a number

--- MathClass.java
+++ MathClass.java
@@ -5,5 +5,8 @@ public class MathClass{
 
         //Print the minimum number
         System.out.println(Math.min(3, 9));
+
+        //Print the Square root of a number
+        System.out.println(Math.sqrt(1024));
     }
 }
