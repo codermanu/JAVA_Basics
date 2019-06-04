aeca2419cdce1d680e8ef408cb61444bf1a9ec2b
codermanu <manu.nevish@gmail.com>
2019-06-04 15:14:02 +0530

    Print integers less than 10 using while loop

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop{
+    public static void main(String[] args){
+        //print integers less than 10
+        int i = 0;
+        while (i<10){
+            System.out.println(i);
+            i++;
+        }
+
+
+    }
+}
