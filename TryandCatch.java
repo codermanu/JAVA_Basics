add982498400ebc8f23be3a1b7336306ec305992
codermanu <manu.nevish@gmail.com>
2019-06-06 22:47:29 +0530

    Try and catch finally

--- TryandCatch.java
+++ TryandCatch.java
@@ -1,11 +1,12 @@
-public class TryandCatch{
-    public static void main(String[] args){
-        try{
-            int[] numbers = {1,2,3,4,5};
+public class TryandCatch {
+    public static void main(String[] args) {
+        try {
+            int[] numbers = { 1, 2, 3, 4, 5 };
             System.out.println(numbers[10]);
-        }
-        catch (Exception e){
+        } catch (Exception e) {
             System.out.println("An error has occured");
+        } finally {
+            System.out.println("The try and catch is finished");
         }
     }
 }
