60634faca121a73c08ff86f2560fdc35978542d1
codermanu <manu.nevish@gmail.com>
2019-06-19 23:37:12 +0530

    Return a value

---
+++ JavaMethodsint.java
@@ -0,0 +1,16 @@
+public class JavaMethodsint {
+    static int add(int x, int y) {
+        return x + y;
+    }
+
+    static int sub(int x, int y) {
+        return x - y;
+    }
+
+    public static void main(String args[]) {
+        int z = add(9080, 789);
+        int a = sub(9080, 789);
+        System.out.println(z);
+        System.out.println(a);
+    }
+}
