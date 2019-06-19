37624187780a0dbb7940b0259643f97df5ed4042
codermanu <manu.nevish@gmail.com>
2019-06-19 23:08:12 +0530

    Pass information as parameters

--- JavaMethods.java
+++ JavaMethods.java
@@ -1,11 +1,11 @@
 public class JavaMethods {
-    static void myMethod() {
-        System.out.println("I just got executed!");
+    static void myMethod(String fname) {
+        System.out.println(fname + " Karunaratne");
     }
 
     public static void main(String[] args) {
-        myMethod();
-        myMethod();
-        myMethod();
+        myMethod("Nadeesha");
+        myMethod("Casper");
+        myMethod("Kasun");
     }
 }
