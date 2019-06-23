3bead254b1e6884f20f0eb38d6add7cfb51ca328
codermanu <manu.nevish@gmail.com>
2019-06-23 16:37:49 +0530

    Add multiple objects

--- Objects.java
+++ Objects.java
@@ -3,6 +3,8 @@ public class Objects {
 
     public static void main(String[] args) {
         Objects myObj = new Objects();
+        Objects myObj2 = new Objects();
         System.out.println(myObj.x);
+        System.out.println(myObj2.x);
     }
 }
