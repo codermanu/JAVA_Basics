f323ca207d6ce18c73dec0a053f210fcd32c7dc8
codermanu <manu.nevish@gmail.com>
2019-06-23 16:38:53 +0530

    Final variables

--- ClassTwo.java
+++ ClassTwo.java
@@ -1,7 +1,15 @@
 public class ClassTwo {
+    final int y = 25;
 
     public static void main(String[] args) {
         ClassOne Object1 = new ClassOne();
         System.out.println(Object1.x);
+        ClassTwo Object2 = new ClassTwo();
+        System.out.println(Object2.y);
+        ClassTwo Object3 = new ClassTwo();
+        // Object3.y = 40; cannot assign a value to a final variable
+        System.out.println(Object3.y);
+        System.out.println(Object2.y);
+
     }
 }
