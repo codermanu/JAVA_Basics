e7f606e34fd03af0ad22fbfad1b55ab833c02205
codermanu <manu.nevish@gmail.com>
2019-06-23 16:31:39 +0530

    Define a method

---
+++ Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    static void method1() {
+        System.out.println("I'm such an asshole");
+    }
+
+    public static void main(String[] args) {
+        method1();
+    }
+}
