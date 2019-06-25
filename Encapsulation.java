9c0fb7c99a217f9f29e146d420928906c472f9ca
codermanu <manu.nevish@gmail.com>
2019-06-25 23:35:35 +0530

    Define private variables inside a class

---
+++ Encapsulation.java
@@ -0,0 +1,7 @@
+public class Encapsulation {
+    public static void main(String[] args) {
+        EncapsulationPerson myObj4 = new EncapsulationPerson();
+        myObj4.setName("Ravindu");
+        System.out.println(myObj4.getName());
+    }
+}
